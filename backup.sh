65f443c8c308e4040fe3f3c51d45fd96801481f2
IuSrnm <andressantiagods47@gmail.com>
2026-04-28 20:00:44 -0500

    - dotfiles update 2026-04-28 20:00

--- backup.sh
+++ backup.sh
@@ -31,7 +31,7 @@ cp ~/.config/fastfetch/bocchi.txt "$DOTFILES/.config/fastfetch"
 cp -r ~/.config/easyeffects/ "$DOTFILES/.config/easyeffects/"
 cp -r ~/.config/copyq/ "$DOTFILES/.config/copyq/"
 cp -r ~/.config/fcitx5/ "$DOTFILES/.config/fcitx5/"
-cp -r ~/.cache/paru/clone/rofi-themes-collection-git/src/rofi-themes-collection/themes/template/rounded-template.rasi "$DOTFILES/.config/rofi/"
+cp -r ~/.config/rofi/template/rounded-template.rasi "$DOTFILES/.config/rofi/template"
 
 # Push to github
 cd "$DOTFILES"
